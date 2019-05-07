2d21e9ff52a74f6a46646154db3c2d7fd8239695
Gleb <Gleb-urencev@yandex.ru>
2019-05-07 11:12:43 +0300

    Добавьте файлы проекта.

---
+++ RecursionListApp/MainForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace RecursionListApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.createButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nodesCountTextBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.removeTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listTextBox
+            // 
+            this.listTextBox.Enabled = false;
+            this.listTextBox.Location = new System.Drawing.Point(66, 19);
+            this.listTextBox.Name = "listTextBox";
+            this.listTextBox.ReadOnly = true;
+            this.listTextBox.ShortcutsEnabled = false;
+            this.listTextBox.Size = new System.Drawing.Size(500, 20);
+            this.listTextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Список:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.createButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.nodesCountTextBox);
+            this.panel1.Location = new System.Drawing.Point(0, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 60);
+            this.panel1.TabIndex = 2;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(369, 21);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 3;
+            this.createButton.Text = "Создать";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Число элементов списка:";
+            // 
+            // nodesCountTextBox
+            // 
+            this.nodesCountTextBox.Location = new System.Drawing.Point(278, 21);
+            this.nodesCountTextBox.Name = "nodesCountTextBox";
+            this.nodesCountTextBox.Size = new System.Drawing.Size(69, 20);
+            this.nodesCountTextBox.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.removeButton);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.removeTextBox);
+            this.panel2.Location = new System.Drawing.Point(0, 173);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(581, 60);
+            this.panel2.TabIndex = 3;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(369, 24);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 7;
+            this.removeButton.Text = "Удалить";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Location = new System.Drawing.Point(82, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Удалить элемент со значением:";
+            // 
+            // removeTextBox
+            // 
+            this.removeTextBox.Location = new System.Drawing.Point(278, 27);
+            this.removeTextBox.Name = "removeTextBox";
+            this.removeTextBox.Size = new System.Drawing.Size(69, 20);
+            this.removeTextBox.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.searchButton);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.searchTextBox);
+            this.panel3.Location = new System.Drawing.Point(0, 107);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(581, 60);
+            this.panel3.TabIndex = 3;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(369, 22);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Найти";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Найти элемент со значением:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(278, 25);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(69, 20);
+            this.searchTextBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 267);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.listTextBox);
+            this.Name = "MainForm";
+            this.Text = "Двунаправленный список";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox listTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nodesCountTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox removeTextBox;
+    }
+}
+
---
+++ RecursionListApp/MainForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace RecursionListApp
+{
+    public partial class MainForm : Form
+    {
+        DoublyLinkedList<int> linkedList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            linkedList = new DoublyLinkedList<int>();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int.TryParse(nodesCountTextBox.Text, out count);
+            
+            //очищаем список
+            linkedList.Clear();
+
+            // добавление элементов
+            // позиция добавляемого элемента в списке, число элементов в списке
+            linkedList.AddRec(1, count);
+            nodesCountTextBox.Text = "";
+            //обновляем поле со списком
+            refreshListTextBox();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            int.TryParse(searchTextBox.Text, out value);
+            if (linkedList.Head != null)
+            {
+                // ссылка на голову, значение для поиска, позиция элемента в списке
+                int index = linkedList.SearchRec(linkedList.Head, value, 1);
+                if (index == -1)
+                {
+                    MessageBox.Show("Элемент со значением " + value + " в списке не найден.");
+                }
+                else
+                {
+                    MessageBox.Show("Найден элемент с позицией " + index + ".");
+                }
+            } else
+            {
+                MessageBox.Show("Список пуст.");
+            }
+            searchTextBox.Text = "";
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            int.TryParse(removeTextBox.Text, out value);
+
+            if (linkedList.Head != null)
+            {
+                // удаление элементов
+                // ссылка на голову, значение для удаления, позиция элемента в списке
+                int index = linkedList.RemoveRec(linkedList.Head, value, 1);
+
+                removeTextBox.Text = "";
+                //обновляем поле со списком
+                refreshListTextBox();
+
+                if (index == -1)
+                {
+                    MessageBox.Show("Элемент со значением " + value + " в списке не найден.");
+                }
+                else
+                {
+                    MessageBox.Show("Удален элемент с позицией " + index + ".");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Список пуст.");
+            }
+        }
+
+        private void refreshListTextBox()
+        {
+            listTextBox.Text = "";
+            foreach (var item in linkedList)
+            {
+                listTextBox.Text += item + " ";
+            }
+        }        
+    }
+
+    public class ListElement<Int>
+    {
+        public ListElement(int data)
+        {
+            Data = data;
+        }
+        public int Data { get; set; }
+        public ListElement<int> Previous { get; set; }
+        public ListElement<int> Next { get; set; }
+    }
+
+    public class DoublyLinkedList<Int> : IEnumerable<int>  // двусвязный список
+    {
+        ListElement<int> head; // головной/первый элемент
+        ListElement<int> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        public ListElement<int> Head { get { return head; } }
+        public int Count { get { return count; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void AddRec(int currElNumber, int maxCount)
+        {
+            if (currElNumber == maxCount + 1) return;
+            else 
+            {
+                ListElement<int> node = new ListElement<int>(currElNumber);
+
+                if (head == null) // первый элемент в начало 
+                    head = node;
+                else // ветка для записи послежющих элементов, которые не равны головному
+                {
+                    tail.Next = node;
+                    node.Previous = tail;
+                }
+                tail = node;
+                currElNumber++; // текущий элемент под запись
+                count++; // счётчик элментов
+
+                AddRec(currElNumber, maxCount);
+            }            
+        }
+
+        public int SearchRec(ListElement<int> currEl, int value, int currElNumber)
+        {
+            //элемент найден - возвращаем позицию
+            if (currEl.Data.Equals(value)) return currElNumber;
+            //условие выхода из рекурсии - конец списка
+            if (currEl.Next == null) return -1;
+            else
+            {
+                currElNumber++;
+                return SearchRec(currEl.Next, value, currElNumber);
+            }
+        }
+
+        public int RemoveRec(ListElement<int> currEl, int value, int currElNumber)
+        {            
+            //найден элемент для удаления
+            if (currEl.Data.Equals(value))
+            {
+                // если узел не последний
+                if (currEl.Next != null)
+                {
+                    currEl.Next.Previous = currEl.Previous;
+                }
+                else
+                {
+                    // если последний, переустанавливаем tail
+                    tail = currEl.Previous;
+                }
+
+                // если узел не первый
+                if (currEl.Previous != null)
+                {
+                    currEl.Previous.Next = currEl.Next;
+                }
+                else
+                {
+                    // если первый, переустанавливаем head
+                    head = currEl.Next;
+                }
+                count--;
+                return currElNumber;
+            }
+
+            //условие выхода из рекурсии - конец списка
+            if (currEl.Next == null) return -1;
+            else
+            {
+                currElNumber++;
+                return RemoveRec(currEl.Next, value, currElNumber);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+         {
+             return ((IEnumerable)this).GetEnumerator();
+         }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            ListElement<int> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
